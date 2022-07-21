baa7cb171cb64790fc4e9b6c8d5eb601b59276da
Alexandra Sapojnic <alexandra.sapojnic@arobs.com>
2022-07-21 12:06:19 +0300

    Conditional Operator

---
+++ ConditionalOperator/src/Operator.java
@@ -0,0 +1,11 @@
+public class Operator {
+    public static void main(String[] args) {
+        int num =13;
+        if((num%2==0) && (num>10 )) {
+            System.out.println("Numarul este par!");
+        }
+        if((num%2!=0) && (num<15)) {
+            System.out.println("Numarul este impar!");
+        }
+    }
+}
