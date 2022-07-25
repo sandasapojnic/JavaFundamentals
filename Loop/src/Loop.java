5d2c1548ca13d5a11190775e1b9e6502793a0632
Alexandra Sapojnic <alexandra.sapojnic@arobs.com>
2022-07-25 12:53:31 +0300

    The sum of numbers

---
+++ Loop/src/Loop.java
@@ -0,0 +1,26 @@
+public class Loop {
+    public static void main(String[] args) {
+        int sum=0;
+        int d=0;
+        do {
+            sum+=d;
+            d++;
+        } while(d<=9);
+
+        System.out.println("The sum of numbers calculated by loop do...while = "+sum);
+
+        sum=0;
+        d=0;
+        while(d<=7){
+            sum+=d;
+            d++;
+        }
+        System.out.println("The sum of numbers calculated by loop while = "+sum);
+        
+        sum=0;
+        for(int i=0;i<=5;i++){
+            sum+=i;
+        }
+        System.out.println("The sum of numbers calculated by loop for  = "+sum);
+    }
+}
