1c9dd065ced0d24fb1c1f78e174f8ecc2ee7604d
Alexandra Sapojnic <alexandra.sapojnic@arobs.com>
2022-08-02 11:36:48 +0300

    Reverse string

---
+++ src/Reverse.java
@@ -0,0 +1,17 @@
+public class Reverse {
+    String str1="";
+    public String reverseString(String s){
+        char chr1;
+        for(int i=s.length()-1;i>=0;i--){
+         chr1= s.charAt(i);
+         str1+=chr1;
+        }
+         return str1;
+    }
+
+    public static void main(String[] args) {
+        Reverse r =new Reverse();
+        System.out.print(r.reverseString("marti"));
+
+    }
+}
