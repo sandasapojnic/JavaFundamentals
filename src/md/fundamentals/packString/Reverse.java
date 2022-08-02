837d973a51c1a5624691d94d4489b07c0f21d1e5
Alexandra Sapojnic <alexandra.sapojnic@arobs.com>
2022-08-02 13:10:59 +0300

    new  commit

--- src/md/fundamentals/packString/Reverse.java
+++ src/md/fundamentals/packString/Reverse.java
@@ -1,12 +1,11 @@
 package md.fundamentals.packString;
 
 public class Reverse {
-    private String str1="";
+
     public String reverseString(String s){
-        char chr1;
+        String str1="";
         for(int i=s.length()-1;i>=0;i--){
-         chr1= s.charAt(i);
-         str1+=chr1;
+         str1+= s.charAt(i);
         }
          return str1;
     }
@@ -14,6 +13,5 @@ public class Reverse {
     public static void main(String[] args) {
         Reverse r =new Reverse();
         System.out.print(r.reverseString("marti"));
-
     }
 }
